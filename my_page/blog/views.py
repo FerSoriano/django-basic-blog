1a2fb0fde90f0c115e7f9091f400b5c4c5a68870
FerSoriano <fer8f10@hotmail.com>
2025-07-16 13:51:59 -0600

    new logic in search_by_user()

--- my_page/blog/views.py
+++ my_page/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponseNotFound
 from django.urls import reverse
 from .models import Post
 
@@ -21,24 +21,50 @@ def home(request):
 
 
 def search_by_user(request):
+    redirect_html = 'blog/search_user.html'
     if request.method == 'POST':
-        user = request.POST.get('user')
-        redirect_path = reverse('posts_by_user', args=[user])
-        return HttpResponseRedirect(redirect_path)
-    return render(request, 'blog/search_user.html')
+        action = request.POST.get('action')
+        user = request.POST.get('user')  # obtenemos el usuario que mandan desde el HTML # noqa
+
+        if action == 'search':
+            posts = Post.objects.filter(user__iexact=user).order_by('-fecha')
+
+            if not posts.exists():
+                return render(
+                    request, redirect_html,
+                    {
+                        'users': get_users(),
+                        'show_posts': False,
+                        'user': user,
+                        'user_not_found': True
+                    })
+
+            return render(
+                request, redirect_html,
+                {
+                    'user': user,
+                    'posts': posts,
+                    'show_posts': True
+                })
+
+    return render(
+        request, redirect_html,
+        {
+            'users': get_users(),
+            'show_posts': False
+        })
 
 
-def show_post_by_user(request, user):
-    try:
-        posts = Post.objects.filter(user__iexact=user).order_by('-fecha')
-        return render(
-            request, 'blog/posts_by_user.html',
-            {
-                'user': user,
-                'posts': posts
-            })
-    except KeyError:
-        return HttpResponseNotFound('{"ErrorMessage": "Invalid user"}')
+def get_users():
+    users = Post.objects.values_list('user', flat=True).distinct()
+    users_list = []
+    distinct = set()
+    for user in users:
+        lower_user = user.lower()
+        if lower_user not in distinct:
+            distinct.add(lower_user)
+            users_list.append(user)
+    return sorted(users_list)
 
 
 def show_all_post(request):
@@ -47,7 +73,7 @@ def show_all_post(request):
 
 
 def delete_post_by_id(request):
-    posts = Post.objects.all().order_by('-fecha')[:15]  # ultimos 15
+    posts = Post.objects.all().order_by('-fecha')[:10]  # ultimos 10
     if request.method == 'POST':
         try:
             post_id = request.POST.get('post_id')
@@ -64,7 +90,7 @@ def delete_post_by_id(request):
                     'deleted_user': post_data['usuario']
                 })
 
-        except Post.DoesNotExist:
+        except (Post.DoesNotExist, ValueError):
             return HttpResponseNotFound("El post NO existe. Intenta de nuevo.")
     # Si es GET, muestra todos los posts
     return render(request, 'blog/delete_posts.html', {'posts': posts})
